a471ace28759fc7082755a383988964c00cac94a
yosifshaban6 <yosifshaban6@gmail.com>
2025-06-17 16:47:26 +0300

    debloy: Write vercel.json for deployment configurations

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: "dist", // Explicitly set output directory
+    emptyOutDir: true, // Clear the directory before build
+    sourcemap: true, // Helpful for debugging
+  },
 });
